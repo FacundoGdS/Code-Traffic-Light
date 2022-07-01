24f0038478d0c3c648b971626a5d452da1f276a0
Facundo <facundogds@gmail.com>
2022-07-01 23:41:15 +0000

    Instrucciones y fin

--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -2,29 +2,44 @@ import React, { useState } from "react";
 import Akitu from "../../img/akitu-kart.png";
 
 const TrafficLight = () => {
-	const [color, setColor] = useState("");
+  const [color, setColor] = useState("");
 
-	const redHandler = () => {
-		setColor("red");
-	};
-	const yellowHandler = () => {
-		setColor("yellow");
-	};
-	const greenHandler = () => {
-		setColor("green");
-	};
+  const redHandler = () => {
+    setColor("red");
+  };
+  const yellowHandler = () => {
+    setColor("yellow");
+  };
+  const greenHandler = () => {
+    setColor("green");
+  };
 
-	return (
-		<>
-		<div className="top"></div>
-		<div className="semaphore">
-			<div onClick={redHandler} className={"light red" + (color === "red" ? " shine1": "")}></div>
-			<div onClick={yellowHandler} className={"light yellow" + (color === "yellow" ? " shine2": "")}></div>
-			<div onClick={greenHandler} className={"light green" + (color === "green" ? " shine3": "")}></div>
-		</div>
-		<img className="img" src={Akitu} />
-		</>
-	);
+  return (
+    <>
+      <div className="top"></div>
+      <div className="semaphore">
+        <div
+          onClick={redHandler}
+          className={"light red" + (color === "red" ? " shine1" : "")}
+        ></div>
+        <div
+          onClick={yellowHandler}
+          className={"light yellow" + (color === "yellow" ? " shine2" : "")}
+        ></div>
+        <div
+          onClick={greenHandler}
+          className={"light green" + (color === "green" ? " shine3" : "")}
+        ></div>
+      </div>
+      <div className="centered">
+	  <img className="img" src={Akitu} />
+      <div className="vineta">
+        <p> &nbsp;<b>Instrucciones:</b></p>
+		<p> &nbsp;Por favor, elige un color del semáforo &nbsp;para activar su resplandor.</p>
+      </div>
+	  </div>
+    </>
+  );
 };
 
 export default TrafficLight;
